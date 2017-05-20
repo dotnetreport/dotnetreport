811e739730d5532d4f065a188ecb2498c7a7c39d
razisyed <syed_razi@hotmail.com>
2017-05-20 10:55:42 -0500

    init code commit

---
+++ Controllers/ReportController.cs
@@ -0,0 +1,498 @@
+﻿using ReportBuilder.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+using System.Net.Http;
+using System.Threading.Tasks;
+using OfficeOpenXml;
+using System.Text.RegularExpressions;
+
+namespace ReportBuilder.Web.Controllers
+{
+    public class ReportController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Report(int reportId, string reportName, string reportDescription, bool includeSubTotal,
+            bool aggregateReport, bool showDataWithGraph, string reportSql, string connectKey, string reportFilter, string reportType, int selectedFolder)
+        {
+            var model = new DotNetReportModel
+            {
+                ReportId = reportId,
+                ReportType = reportType,
+                ReportName = HttpUtility.UrlDecode(reportName),
+                ReportDescription = HttpUtility.UrlDecode(reportDescription),
+                ReportSql = reportSql,
+                ConnectKey = connectKey,
+                IncludeSubTotals = includeSubTotal,
+                ShowDataWithGraph = showDataWithGraph,
+                SelectedFolder = selectedFolder,
+                ReportFilter = reportFilter // json data to setup filter correctly again
+            };
+
+            return View(model);
+        }
+
+        public JsonResult GetLookupList(string lookupSql, string connectKey)
+        {
+            var sql = Decrypt(lookupSql);
+
+            // Uncomment if you want to restrict max records returned
+            sql = sql.Replace("SELECT ", "SELECT TOP 500 ");
+            
+            var json = new StringBuilder();
+            var dt = new DataTable();
+            using (var conn = new SqlConnection(ConfigurationManager.ConnectionStrings[connectKey].ConnectionString))
+            {
+                conn.Open();
+                var command = new SqlCommand(sql, conn);
+                var adapter = new SqlDataAdapter(command);
+
+                adapter.Fill(dt);
+            }
+
+            int i=0;
+            foreach (DataRow dr in dt.Rows)
+            {
+                json.AppendFormat("{{\"id\": \"{0}\", \"text\": \"{1}\"}}{2}", dr[0], dr[1], i!=dt.Rows.Count-1 ? "," : "");
+                i+=1;            
+            }
+
+            return Json((new JavaScriptSerializer()).DeserializeObject("["+json.ToString()+"]"), JsonRequestBehavior.AllowGet);
+        }
+
+
+
+        public JsonResult RunReport(string reportSql, string connectKey, string reportType, int pageNumber = 1, int pageSize = 50, string sortBy = null, bool desc = false)
+        {
+            var sql = Decrypt(reportSql);
+
+            try
+            {
+                if (!String.IsNullOrEmpty(sortBy))
+                {
+                    if (sortBy.StartsWith("DATENAME(MONTH, "))
+                    {
+                        sortBy = sortBy.Replace("DATENAME(MONTH, ", "MONTH(");
+                    }
+                    sql = sql.Substring(0, sql.IndexOf("ORDER BY")) + "ORDER BY " + sortBy + (desc ? " DESC" : "");
+                }
+
+                // Execute sql
+                var dt = new DataTable();
+                var dtPaged = new DataTable();
+                using (var conn = new SqlConnection(ConfigurationManager.ConnectionStrings[connectKey].ConnectionString))
+                {
+                    conn.Open();
+                    var command = new SqlCommand(sql, conn);
+                    var adapter = new SqlDataAdapter(command);
+
+                    adapter.Fill(dt);
+                }
+
+                dtPaged = (dt.Rows.Count > 0) ? dtPaged = dt.AsEnumerable().Skip((pageNumber - 1) * pageSize).Take(pageSize).CopyToDataTable(): dt;                
+
+                var model = new DotNetReportResultModel
+                {
+                    ReportData = DataTableToDotNetReportDataModel(dtPaged, sql),
+                    Warnings = GetWarnings(sql),
+                    ReportSql = sql,
+                    ReportDebug = Request.Url.Host.Contains("localhost"),
+                    Pager = new DotNetReportPagerModel
+                    {
+                        CurrentPage = pageNumber, 
+                        PageSize = pageSize,
+                        TotalRecords = dt.Rows.Count,
+                        TotalPages =(int) ((dt.Rows.Count/pageSize)+1)
+                    }
+                };
+
+                return new JsonResult()
+                {
+                    Data = model,
+                    JsonRequestBehavior = JsonRequestBehavior.AllowGet,
+                    MaxJsonLength = Int32.MaxValue
+                };
+
+            }
+
+            catch (Exception ex)
+            {
+                var model = new DotNetReportResultModel
+                {
+                    ReportSql = sql,
+                    HasError=true,
+                    Exception = ex.Message
+                };
+
+                return Json(model, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult DownloadExcel(string reportSql, string connectKey, string reportName)
+        {
+            var sql = Decrypt(reportSql);
+
+            // Execute sql
+            var dt = new DataTable();
+            using (var conn = new SqlConnection(ConfigurationManager.ConnectionStrings[connectKey].ConnectionString))
+            {
+                conn.Open();
+                var command = new SqlCommand(sql, conn);
+                var adapter = new SqlDataAdapter(command);
+
+                adapter.Fill(dt);
+            }
+
+            
+            Response.ClearContent();
+
+            using (ExcelPackage xp = new ExcelPackage())
+            {
+
+                ExcelWorksheet ws = xp.Workbook.Worksheets.Add(reportName);
+
+                int rowstart = 1;
+                int colstart = 1;
+                int rowend = rowstart;
+                int colend = dt.Columns.Count;
+
+                ws.Cells[rowstart, colstart, rowend, colend].Merge = true;
+                ws.Cells[rowstart, colstart, rowend, colend].Value = reportName;
+                ws.Cells[rowstart, colstart, rowend, colend].Style.Font.Bold = true;
+                ws.Cells[rowstart, colstart, rowend, colend].Style.Font.Size = 14;
+
+                rowstart += 2;
+                rowend = rowstart + dt.Rows.Count;
+                ws.Cells[rowstart, colstart].LoadFromDataTable(dt, true);
+                ws.Cells[rowstart, colstart, rowstart, colend].Style.Font.Bold = true;
+
+                int i = 1;
+                foreach (DataColumn dc in dt.Columns)
+                {
+                    if (dc.DataType == typeof(decimal))
+                        ws.Column(i).Style.Numberformat.Format = "#0.00";
+
+                    if (dc.DataType == typeof(DateTime))
+                        ws.Column(i).Style.Numberformat.Format = "dd/mm/yyyy";
+
+                    i++;
+                }
+                ws.Cells[ws.Dimension.Address].AutoFitColumns();
+
+
+                Response.AddHeader("content-disposition", "attachment; filename=" + reportName + ".xlsx");
+                Response.ContentType = "application/vnd.ms-excel";
+                Response.BinaryWrite(xp.GetAsByteArray());
+                Response.End();
+
+            }
+
+            
+            Response.End();
+            
+            return View();
+        }
+
+
+        /// <summary>
+        /// Method to Deycrypt encrypted sql statement. PLESE DO NOT CHANGE THIS METHOD
+        /// </summary>
+        private string Decrypt(string encryptedText)
+        {
+
+            byte[] initVectorBytes = Encoding.ASCII.GetBytes("yk0z8f39lgpu70gi"); // PLESE DO NOT CHANGE THIS KEY
+            int keysize = 256;
+
+            byte[] cipherTextBytes = Convert.FromBase64String(encryptedText.Replace("%3D","="));
+            var passPhrase = ConfigurationManager.AppSettings["dotNetReport.privateApiToken"].ToLower();
+            using (PasswordDeriveBytes password = new PasswordDeriveBytes(passPhrase, null))
+            {
+                byte[] keyBytes = password.GetBytes(keysize / 8);
+                using (RijndaelManaged symmetricKey = new RijndaelManaged())
+                {
+                    symmetricKey.Mode = CipherMode.CBC;
+                    using (ICryptoTransform decryptor = symmetricKey.CreateDecryptor(keyBytes, initVectorBytes))
+                    {
+                        using (MemoryStream memoryStream = new MemoryStream(cipherTextBytes))
+                        {
+                            using (CryptoStream cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read))
+                            {
+                                byte[] plainTextBytes = new byte[cipherTextBytes.Length];
+                                int decryptedByteCount = cryptoStream.Read(plainTextBytes, 0, plainTextBytes.Length);
+                                return Encoding.UTF8.GetString(plainTextBytes, 0, decryptedByteCount);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+
+        private string GetWarnings(string sql)
+        {
+            var warning = "";
+            if (sql.ToLower().Contains("cross join"))
+            {
+                warning += "Some data used in this report have relations that are not setup properly, so data might duplicate incorrectly.<br/>";
+            }
+
+            return warning;
+        }
+
+        public static bool IsNumericType(Type type)
+        {
+
+            switch (Type.GetTypeCode(type))
+            {
+                case TypeCode.Int16:
+                case TypeCode.UInt16:
+                case TypeCode.Int32:
+                case TypeCode.UInt32:
+                case TypeCode.Int64:
+                case TypeCode.UInt64:
+                case TypeCode.Single:
+                case TypeCode.Double:
+                case TypeCode.Decimal:
+                    return true;
+
+                case TypeCode.Boolean:
+                case TypeCode.DateTime:
+                case TypeCode.String:
+                default:
+                    return false;
+            }
+        }
+
+        public static string GetLabelValue(DataColumn col, DataRow row)
+        {
+            switch (Type.GetTypeCode(col.DataType))
+            {
+                case TypeCode.Int16:
+                case TypeCode.UInt16:
+                case TypeCode.Int32:
+                case TypeCode.UInt32:
+                case TypeCode.Int64:
+                case TypeCode.UInt64:
+                case TypeCode.Single:
+                    return row[col].ToString();
+
+                case TypeCode.Double:
+                case TypeCode.Decimal:
+                    return @row[col].ToString();// "'" + (Convert.ToDouble(@row[col].ToString()).ToString("C")) + "'";
+
+                case TypeCode.Boolean:
+                    return (Convert.ToBoolean(@row[col]) ? "Yes" : "No");
+
+                case TypeCode.DateTime:
+                    try
+                    {
+                        return "'" + @Convert.ToDateTime(@row[col]).ToShortDateString() + "'";
+                    }
+                    catch
+                    {
+                        return "'" + @row[col] + "'";
+                    }
+
+                case TypeCode.String:
+                default:
+                    return "'" + @row[col].ToString().Replace("'", "") + "'";
+            }
+        }
+
+        public static string GetFormattedValue(DataColumn col, DataRow row)
+        {
+            if (@row[col] != null)
+            {
+                switch (Type.GetTypeCode(col.DataType))
+                {
+                    case TypeCode.Int16:
+                    case TypeCode.UInt16:
+                    case TypeCode.Int32:
+                    case TypeCode.UInt32:
+                    case TypeCode.Int64:
+                    case TypeCode.UInt64:
+                    case TypeCode.Single:
+                        return row[col].ToString();
+                                        
+
+                    case TypeCode.Double:
+                    case TypeCode.Decimal:
+                    return Convert.ToDouble(row[col].ToString()).ToString("C");
+                                        
+
+                    case TypeCode.Boolean:
+                    return (Convert.ToBoolean(row[col]) ? "Yes" : "No");
+                                        
+
+                    case TypeCode.DateTime:
+                        try
+                        {
+                            return Convert.ToDateTime(row[col]).ToShortDateString();
+                        }
+                        catch
+                        {
+                            return row[col] != null ? row[col].ToString() : null;
+                        }
+                                        
+                    case TypeCode.String:
+                    default:
+                        if (row[col].ToString() == "System.Byte[]")
+                        {
+                                            
+                            return "<img src=\"data:image/png;base64," + Convert.ToBase64String((byte[])row[col],0, ((byte[])row[col]).Length)+ "\" style=\"max-width: 200px;\" />";
+                        }
+                        else
+                        {
+                            return row[col].ToString();
+                        }
+                                        
+                }
+            }
+            return "";
+        }
+
+        private DotNetReportDataModel DataTableToDotNetReportDataModel(DataTable dt, string sql)
+        {
+            var model = new DotNetReportDataModel
+            {
+                Columns = new List<DotNetReportDataColumnModel>(),
+                Rows = new List<DotNetReportDataRowModel>()
+            };
+
+            sql = sql.Substring(0, sql.IndexOf("FROM")).Replace("SELECT", "").Trim();
+            var sqlFields = Regex.Split(sql, ", (?![^\\(]*?\\))");
+
+            int i = 0;
+            foreach (DataColumn col in dt.Columns)
+            {
+                var sqlField = sqlFields[i++];
+                model.Columns.Add(new DotNetReportDataColumnModel
+                {
+                    SqlField = sqlField.Substring(0, sqlField.IndexOf("AS")).Trim(),
+                    ColumnName = col.ColumnName,
+                    DataType = col.DataType.ToString(),
+                    IsNumeric = IsNumericType(col.DataType)
+                });
+            }
+
+            foreach (DataRow row in dt.Rows)
+            {
+                i = 0;
+                var items = new List<DotNetReportDataRowItemModel>();
+
+                foreach (DataColumn col in dt.Columns)
+                {
+                    items.Add(new DotNetReportDataRowItemModel
+                    {
+                        Column = model.Columns[i],
+                        Value = row[col] != null ? row[col].ToString() : null,
+                        FormattedValue = GetFormattedValue(col, row),
+                        LabelValue = GetLabelValue(col, row)
+                    });
+                    i += 1;
+                }
+
+                model.Rows.Add(new DotNetReportDataRowModel
+                {
+                    Items = items.ToArray()
+                });
+            }
+
+            return model;
+        }
+
+
+    }
+    
+}
+
+namespace ReportBuilder.Web
+{
+    public static class ReportUtil
+    {
+        /// <summary>
+        /// Get script file name with available version
+        /// </summary>
+        /// <param name="expression"></param>
+        /// <returns></returns>
+        public static string GetScriptFile(string expression)
+        {
+            var path = HttpRuntime.AppDomainAppPath;
+            var files = Directory.GetFiles(path + "Scripts").Select(x => Path.GetFileName(x)).ToList();
+            string script = string.Empty;
+            expression = expression.Replace(".", @"\.").Replace("{0}", "(\\d+\\.?)+");
+            var r = new System.Text.RegularExpressions.Regex(@expression, System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+
+            foreach (var f in files)
+            {
+                var m = r.Match(f);
+                while (m.Success)
+                {
+                    script = m.Captures[0].ToString();
+
+                    m = m.NextMatch();
+                }
+            }
+
+            return script;
+        }
+    }
+
+    public static class HtmlExtensions
+    {
+
+        private class ScriptBlock : IDisposable
+        {
+            private const string scriptsKey = "scripts";
+            public static List<string> pageScripts
+            {
+                get
+                {
+                    if (HttpContext.Current.Items[scriptsKey] == null)
+                        HttpContext.Current.Items[scriptsKey] = new List<string>();
+                    return (List<string>)HttpContext.Current.Items[scriptsKey];
+                }
+            }
+
+            WebViewPage webPageBase;
+
+            public ScriptBlock(WebViewPage webPageBase)
+            {
+                this.webPageBase = webPageBase;
+                this.webPageBase.OutputStack.Push(new StringWriter());
+            }
+
+            public void Dispose()
+            {
+                pageScripts.Add(((StringWriter)this.webPageBase.OutputStack.Pop()).ToString());
+            }
+        }
+
+        public static IDisposable BeginScripts(this HtmlHelper helper)
+        {
+            return new ScriptBlock((WebViewPage)helper.ViewDataContainer);
+        }
+
+        public static MvcHtmlString PageScripts(this HtmlHelper helper)
+        {
+            return MvcHtmlString.Create(string.Join(Environment.NewLine, ScriptBlock.pageScripts.Select(s => s.ToString())));
+        }
+
+    }
+}
+
+
---
+++ Scripts/dotnetreport-helper.js
@@ -0,0 +1,157 @@
+﻿/// .Net Report Builder helper methods
+
+// Ajax call wrapper function
+function ajaxcall(options) {
+    if ($.blockUI) {
+        $.blockUI({ baseZ: 500 });
+    }
+
+    return $.ajax({
+        url: options.url,
+        type: options.type || "GET",
+        data: options.data,
+        cache: options.cache || false,
+        dataType: options.dataType || "json",
+        contentType: options.contentType || "application/json; charset=utf-8",
+        headers: options.headers || {}
+    }).success(function (data) {
+        if ($.unblockUI) {
+            $.unblockUI();
+        }
+        delete options;
+    }).fail(function (jqxhr, status, error) {
+        if ($.unblockUI) {
+            $.unblockUI();
+        }
+        delete options;
+        var msg = jqxhr.responseJSON && jqxhr.responseJSON.Message ? "\n" + jqxhr.responseJSON.Message : "";
+
+        if (error == "Conflict") {
+            toastr.error("Conflict detected. Please ensure the record is not a duplicate and that it has no related records." + msg);
+        } else if (error == "Bad Request") {
+            toastr.error("Validation failed for your request. Please make sure the data provided is correct." + msg);
+        } else if (error == "Unauthorized") {
+            toastr.error("You are not authorized to make that request." + msg);
+        } else if (error == "Forbidden") {
+            location.reload(true);
+        } else if (error == "Not Found") {
+            toastr.error("Record not found." + msg);
+        } else if (error == "Internal Server Error") {
+            toastr.error("The system was unable to complete your request." + msg);
+        } else {
+            toastr.error(status + ": " + msg);
+        }
+    });
+}
+
+// knockout binding extenders
+ko.bindingHandlers.datepicker = {
+    init: function (element, valueAccessor, allBindingsAccessor) {
+        //initialize datepicker with some optional options
+        var options = allBindingsAccessor().datepickerOptions || {};
+        $(element).datepicker(options);
+
+        //handle the field changing
+        ko.utils.registerEventHandler(element, "change", function () {
+            var observable = valueAccessor();
+            observable($(element).datepicker({ dateFormat: 'mm/dd/yyyy' }).val());
+        });
+
+        //handle disposal (if KO removes by the template binding)
+        ko.utils.domNodeDisposal.addDisposeCallback(element, function () {
+            $(element).datepicker("destroy");
+        });
+
+    },
+    //update the control when the view model changes
+    update: function (element, valueAccessor) {
+        var value = ko.utils.unwrapObservable(valueAccessor()),
+            current = $(element).datepicker("getDate");
+
+        if (value - current !== 0) {
+            $(element).datepicker("setDate", value);
+        }
+    }
+};
+
+ko.bindingHandlers.fadeVisible = {
+    init: function (element, valueAccessor) {
+        // Initially set the element to be instantly visible/hidden depending on the value
+        var value = valueAccessor();
+        $(element).toggle(ko.utils.unwrapObservable(value)); // Use "unwrapObservable" so we can handle values that may or may not be observable
+    },
+    update: function (element, valueAccessor) {
+        // Whenever the value subsequently changes, slowly fade the element in or out
+        var value = valueAccessor();
+        ko.utils.unwrapObservable(value) ? $(element).fadeIn("slow") : $(element).hide();
+    }
+};
+
+ko.bindingHandlers.checkedInArray = {
+    init: function (element, valueAccessor) {
+        ko.utils.registerEventHandler(element, "click", function () {
+            var options = ko.utils.unwrapObservable(valueAccessor()),
+                array = options.array, // don't unwrap array because we want to update the observable array itself
+                value = ko.utils.unwrapObservable(options.value),
+                checked = element.checked;
+
+            ko.utils.addOrRemoveItem(array, value, checked);
+
+        });
+    },
+    update: function (element, valueAccessor) {
+        var options = ko.utils.unwrapObservable(valueAccessor()),
+            array = ko.utils.unwrapObservable(options.array),
+            value = ko.utils.unwrapObservable(options.value);
+
+        element.checked = ko.utils.arrayIndexOf(array, value) >= 0;
+    }
+};
+
+ko.bindingHandlers.select2 = {
+    after: ["options", "value"],
+    init: function (el, valueAccessor, allBindingsAccessor, viewModel) {
+        $(el).select2(ko.unwrap(valueAccessor()));
+        ko.utils.domNodeDisposal.addDisposeCallback(el, function () {
+            $(el).select2('destroy');
+        });
+    },
+    update: function (el, valueAccessor, allBindingsAccessor, viewModel) {
+        var allBindings = allBindingsAccessor();
+        var select2 = $(el).data("select2");
+        if ("value" in allBindings) {
+            var newValue = "" + ko.unwrap(allBindings.value);
+            if ((allBindings.select2.multiple || el.multiple) && newValue.constructor !== Array) {
+                select2.val([newValue.split(",")]);
+            }
+            else {
+                select2.val([newValue]);
+            }
+        }
+    }
+};
+
+function redirectToReport(url, prm, newtab) {
+    prm = (typeof prm == 'undefined') ? {} : prm;
+    newtab = (typeof newtab == 'undefined') ? false : newtab;
+
+    var form = document.createElement("form");
+    $(form).attr("id", "reg-form").attr("name", "reg-form").attr("action", url).attr("method", "post").attr("enctype", "multipart/form-data");
+    if (newtab) {
+        $(form).attr("target", "_blank");
+    }
+    $.each(prm, function (key) {
+        $(form).append('<input type="text" name="' + key + '" value="' + escape(this) + '" />');
+    });
+    document.body.appendChild(form);
+    form.submit();
+    document.body.removeChild(form);
+
+    return false;
+}
+
+function htmlDecode(input){
+  var e = document.createElement('div');
+  e.innerHTML = input;
+  return e.childNodes.length === 0 ? "" : e.childNodes[0].nodeValue;
+}
