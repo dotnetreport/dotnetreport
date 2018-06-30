f09deaad93d50ffc10c9ade1420aecb2aa2f7e18
admin@dotnetreport.com <admin@dotnetreport.com>
2018-06-30 02:55:15 -0500

    fixed js issue with loading filter

--- Scripts/dotnetreport.js
+++ Scripts/dotnetreport.js
@@ -1,4 +1,4 @@
-﻿/// .Net Report Builder view model v2.0.0
+﻿/// .Net Report Builder view model v2.0.2
 /// License has to be purchased for use
 /// 2015-2017 (c) www.dotnetreport.com
 
@@ -905,7 +905,7 @@ var reportViewModel = function (options) {
 			function addSavedFilters() {
 				$.each(report.Filters, function (i, e) {
 					if (filterFieldsOnFly.indexOf(e.FieldId) < 0) {
-						var onFly = $.grep(self.SelectedFields(), function (x) { return x.filterOnFly() == true && x.fieldId == e.FieldId; });
+						var onFly = $.grep(self.SelectedFields(), function (x) { return x.filterOnFly() == true && x.fieldId == e.FieldId; }).length > 0;
 						if (onFly) filterFieldsOnFly.push(e.FieldId);
 						self.AddFilter(e, onFly)
 					}
