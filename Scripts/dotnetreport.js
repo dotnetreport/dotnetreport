c2a03ddf59b99f6923ee51ab71a7c9b23c3938ae
Razi Syed <admin@dotnetreport.com>
2021-12-09 07:15:53 -0600

    updated format number function

--- Scripts/dotnetreport.js
+++ Scripts/dotnetreport.js
@@ -2408,18 +2408,9 @@ var reportViewModel = function (options) {
 		return false;
 	};
 
-	self.formatNumber = function (number, decPlaces, decSep, thouSep) {
-		decPlaces = isNaN(decPlaces = Math.abs(decPlaces)) ? 2 : decPlaces,
-			decSep = typeof decSep === "undefined" ? "." : decSep;
-		thouSep = typeof thouSep === "undefined" ? "," : thouSep;
-		var sign = number < 0 ? "-" : "";
-		var i = String(parseInt(number = Math.abs(Number(number) || 0).toFixed(decPlaces)));
-		var j = (j = i.length) > 3 ? j % 3 : 0;
-
-		return sign +
-			(j ? i.substr(0, j) + thouSep : "") +
-			i.substr(j).replace(/(\decSep{3})(?=\decSep)/g, "$1" + thouSep) +
-			(decPlaces ? decSep + Math.abs(number - i).toFixed(decPlaces).slice(2) : "");
+	self.formatNumber = function (number, decPlaces) {
+		decPlaces = isNaN(decPlaces = Math.abs(decPlaces)) ? 2 : decPlaces;
+		return parseFloat(number).toFixed(decPlaces).replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 	}
 
 	// ui-validation
