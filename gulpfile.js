348c6016a0cfb107de482e8a6eb20b8e70f0f480
Razi Syed <admin@dotnetreport.com>
2022-08-21 16:37:57 -0500

    added files needed for nuget package

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+﻿var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var rimraf = require("rimraf");
+var merge = require('merge-stream');
+
+gulp.task("minify", function () {
+
+	return gulp.src('Scripts/dotnetreport.js')
+		// Minify the file
+		.pipe(uglify())
+		.pipe(concat("dotnetreport.min.js"))
+		// Output
+		.pipe(gulp.dest('Scripts/'));
+});
+
