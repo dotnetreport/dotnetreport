f1f7cdc3c946e895dc32801cc47129808a797c52
Razi Syed <admin@dotnetreport.com>
2024-05-15 09:43:32 -0500

    added win service project for job

---
+++ DotnetReportJob.WinService/ReportScheduleService.cs
@@ -0,0 +1,44 @@
+﻿using Quartz;
+using Quartz.Impl;
+using ReportBuilder.Web.Jobs;
+using System.ServiceProcess;
+
+namespace DotnetReportJob.WinService
+{
+    public partial class ReportScheduleService : ServiceBase
+    {
+        private IScheduler scheduler;
+        public ReportScheduleService()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnStart(string[] args)
+        {
+            var schedulerFactory = new StdSchedulerFactory();
+            scheduler = await schedulerFactory.GetScheduler();
+            await scheduler.Start();
+
+            IJobDetail job = JobBuilder.Create<DotNetReportJob>()
+                                       .WithIdentity("DotNetReportJob")
+                                       .StoreDurably()
+                                       .Build();
+
+            ITrigger trigger = TriggerBuilder.Create()
+                .WithIdentity("DotNetReportJobTrigger")
+                .StartNow()
+                .WithSimpleSchedule(s => s.WithIntervalInSeconds(60).RepeatForever())
+                .Build();
+
+            await scheduler.ScheduleJob(job, trigger);
+        }
+
+        protected override async void OnStop()
+        {
+            if (scheduler != null)
+            {
+                await scheduler.Shutdown();
+            }
+        }
+    }
+}
