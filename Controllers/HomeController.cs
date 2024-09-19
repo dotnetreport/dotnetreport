5a52b7b22ef1ba77cdc3c5a642d057d8590dfa1d
Razi Syed <admin@dotnetreport.com>
2024-09-19 15:48:12 -0500

    added home page

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ReportBuilder.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
