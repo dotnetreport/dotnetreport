fe261992bec99175bb18214b28d7b6043a8825df
Razi Syed <admin@dotnetreport.com>
2024-12-13 01:59:40 -0600

    allow html in custom fields

--- Scripts/dotnetreport-helper.js
+++ Scripts/dotnetreport-helper.js
@@ -612,8 +612,8 @@ var textQuery = function (options) {
             data: {
                 method: "/ReportApi/ParseQuery",
                 model: JSON.stringify({
-                    token: token,
-                    text: text
+                    token: encodeURIComponent(token),
+                    text: encodeURIComponent(text)
                 })
             }
         });
@@ -677,7 +677,7 @@ var textQuery = function (options) {
             return params.term ? {
                 method: "/ReportApi/ParseQuery",
                 model: JSON.stringify({
-                    token: params.term,
+                    token: encodeURIComponent(params.term),
                     text: ''
                 })
             } : null;
