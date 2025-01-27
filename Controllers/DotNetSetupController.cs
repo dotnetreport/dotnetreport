201378a886e3db68044297f925f910c7eef3940a
Razi Syed <admin@dotnetreport.com>
2025-01-27 00:28:40 -0600

    imports cleanup

--- Controllers/DotNetSetupController.cs
+++ Controllers/DotNetSetupController.cs
@@ -1,14 +1,4 @@
-﻿using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-using ReportBuilder.Web.Models;
-using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Data.OleDb;
-using System.Linq;
-using System.Net.Http;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using System.Web.Mvc;
 
 namespace ReportBuilder.Web.Controllers
