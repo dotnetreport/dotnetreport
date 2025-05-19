c60265bccd70d5e564a7e586e8544daa876ec98c
Adil Waheed <adil.waheed@dotnetreport.com>
2025-05-19 22:17:11 +0500

    add ko.toJs on col

--- Scripts/dotnetreport.js
+++ Scripts/dotnetreport.js
@@ -3175,7 +3175,7 @@ var reportViewModel = function (options) {
 				col.decimalPlacesDigit = col.decimalPlaces ? col.decimalPlaces() : null;
 				col.fieldFormating = col.fieldFormat ? col.fieldFormat() : null;
 				col.IsPivotField = e.IsPivotField ;
-				if (skipColDetails !== true) self.columnDetails.push(col);
+				if (skipColDetails !== true) self.columnDetails.push(ko.toJS(col));
 
 				e.decimalPlaces = col.decimalPlaces || ko.observable();
 				e.currencyFormat = col.currencyFormat || ko.observable();
