c4d1221436a42f5994b68ae68725cd347374e04a
Razi Syed <admin@dotnetreport.com>
2025-06-02 22:56:29 -0500

    typo

--- DotNetReport/ReportService.asmx.cs
+++ DotNetReport/ReportService.asmx.cs
@@ -9,7 +9,6 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web;
-using System.Web.Mvc;
 using System.Web.Script.Serialization;
 using System.Web.Script.Services;
 using System.Web.Services;
