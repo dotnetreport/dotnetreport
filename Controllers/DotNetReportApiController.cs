70180a86c1c6cd05f9490744bcdcc5bd5651dd86
Razi Syed <admin@dotnetreport.com>
2025-08-06 10:28:24 -0500

    improved lookup list select top filter

--- Controllers/DotNetReportApiController.cs
+++ Controllers/DotNetReportApiController.cs
@@ -69,7 +69,7 @@ namespace ReportBuilder.Web.Controllers
             }
 
             // Uncomment if you want to restrict max records returned
-            sql = sql.Replace("SELECT ", "SELECT TOP 500 ");
+            sql = sql.Substring(0, 0) + "SELECT DISTINCT TOP 500 " + sql.Substring(0 + "SELECT ".Length);
 
             var json = new StringBuilder();
             var dt = new DataTable();
