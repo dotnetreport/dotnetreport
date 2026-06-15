668840a4e0e7d9f87b9a9566407ccdb9a76179b9
Razi Syed <admin@dotnetreport.com>
2026-06-15 11:27:36 -0500

    improve chromium dispose

--- Models/DotNetReportModel.cs
+++ Models/DotNetReportModel.cs
@@ -4374,6 +4374,8 @@ namespace ReportBuilder.Web.Models
                 await page.EmulateMediaTypeAsync(MediaType.Screen);
                 await page.EvaluateExpressionAsync("$('.report-inner').css('transform','none')");
                 await page.PdfAsync(pdfFile, pdfOptions);
+                await page.DisposeAsync();
+                await browser.DisposeAsync();
                 return File.ReadAllBytes(pdfFile);
             }
             catch (Exception)
